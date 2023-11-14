7ca1625496ee5f538be8e4bffc95fec90769a7bf
Jan-Ole Stein <45128898+janolestein@users.noreply.github.com>
2023-11-15 00:05:01 +0100

    Some Changes

--- app/src/main/java/com/jole/ridetrackermobdev/model/Ride.java
+++ app/src/main/java/com/jole/ridetrackermobdev/model/Ride.java
@@ -11,21 +11,19 @@ import java.util.List;
 @Entity
 public class Ride {
     @PrimaryKey(autoGenerate = true)
-    public int id;
-    //private static int numberOfRides = 1;
-    public String name;
-    public String description;
-    public String date;
-    public double rideLengthKm;
-    public double averageSpeed;
-    public double totalRideTime;
-    public String imgUrl;
+    private int id;
+    private String name;
+    private String description;
+    private String date;
+    private double rideLengthKm;
+    private double averageSpeed;
+    private double totalRideTime;
+    private String imgUrl;
     @TypeConverters({Converters.class})
-    public List<GeoPoint> geoPoints;
+    private List<GeoPoint> geoPoints;
 
 
     public Ride(String name, String description, String date, double rideLengthKm, double averageSpeed, double totalRideTime, String imgUrl, List<GeoPoint> geoPoints) {
-        //this.id = numberOfRides;
         this.name = name;
         this.description = description;
         this.date = date;
@@ -34,79 +32,95 @@ public class Ride {
         this.totalRideTime = totalRideTime;
         this.imgUrl = imgUrl;
         this.geoPoints = geoPoints;
-        //numberOfRides++;
     }
 
-    public int getId() {
+    public int getId()
+    {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(int id)
+    {
         this.id = id;
     }
 
-    public String getName() {
+    public String getName()
+    {
         return name;
     }
 
-    public void setName(String name) {
+    public void setName(String name)
+    {
         this.name = name;
     }
 
-    public String getDescription() {
+    public String getDescription()
+    {
         return description;
     }
 
-    public void setDescription(String description) {
+    public void setDescription(String description)
+    {
         this.description = description;
     }
 
-    public String getDate() {
+    public String getDate()
+    {
         return date;
     }
 
-    public void setDate(LocalDate date) {
-        this.date = date.toString();
+    public void setDate(String date)
+    {
+        this.date = date;
     }
 
-    public double getRideLengthKm() {
+    public double getRideLengthKm()
+    {
         return rideLengthKm;
     }
 
-    public void setRideLengthKm(double rideLengthKm) {
+    public void setRideLengthKm(double rideLengthKm)
+    {
         this.rideLengthKm = rideLengthKm;
     }
 
-    public String getImgUrl() {
-        return imgUrl;
-    }
-
-    public void setImgUrl(String imgUrl) {
-        this.imgUrl = imgUrl;
-    }
-
-    public double getAverageSpeed() {
+    public double getAverageSpeed()
+    {
         return averageSpeed;
     }
 
-    public void setAverageSpeed(double averageSpeed) {
+    public void setAverageSpeed(double averageSpeed)
+    {
         this.averageSpeed = averageSpeed;
     }
 
-    public double getTotalRideTime() {
+    public double getTotalRideTime()
+    {
         return totalRideTime;
     }
 
-    public void setTotalRideTime(double totalRideTime) {
+    public void setTotalRideTime(double totalRideTime)
+    {
         this.totalRideTime = totalRideTime;
     }
 
-//    public static int getNumberOfRides()
-//    {
-//        return numberOfRides;
-//    }
+    public String getImgUrl()
+    {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl)
+    {
+        this.imgUrl = imgUrl;
+    }
+
     public List<GeoPoint> getGeoPoints()
     {
         return geoPoints;
+    }
+
+    public void setGeoPoints(List<GeoPoint> geoPoints)
+    {
+        this.geoPoints = geoPoints;
     }
 }

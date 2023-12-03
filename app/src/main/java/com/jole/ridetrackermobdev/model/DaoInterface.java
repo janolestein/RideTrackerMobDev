c2430638e0384b3567a3924860fd9a4a60268e8d
Jan-Ole Stein <janolestein@gmail.com>
2023-12-03 22:36:34 +0100

    Some Chnages

--- app/src/main/java/com/jole/ridetrackermobdev/model/DaoInterface.java
+++ app/src/main/java/com/jole/ridetrackermobdev/model/DaoInterface.java
@@ -12,13 +12,32 @@ import java.util.Optional;
 @Dao
 public interface DaoInterface
 {
-
+    /**
+     * Adds a new Ride to the Database
+     * @param ride Object to be added to the DB
+     */
     @Insert
     void addNewRide(Ride ride);
+
+    /**
+     * Removes a Ride from the Database
+     * @param ride object ro to be removed
+     */
     @Delete
     void removeRide(Ride ride);
+
+    /**
+     * Gets a List of all Ride Objects in the Database
+     * @return List of all Rides as LiveData<List>
+     */
     @Query("SELECT * FROM Ride")
     LiveData<List<Ride>> getAllRidesList();
+
+    /**
+     * Searches for a Ride with a Id and returns the Ride
+     * @param id of the ride to be retrieved
+     * @return Optional of Ride Object
+     */
     @Query("SELECT * FROM Ride WHERE id LIKE :id LIMIT 1")
     Optional<Ride> findRideById(int id);
 
